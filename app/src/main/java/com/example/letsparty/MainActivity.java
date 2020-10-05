deb6035a8582f13e1768b315ce57cba0df1ff8e3
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2020-10-05 21:17:23 +1100

    Changed basic build files so it's more compatible for more devices

--- app/src/main/java/com/example/letsparty/MainActivity.java
+++ app/src/main/java/com/example/letsparty/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.letsparty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.letsparty.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.menuHost.setOnClickListener(view -> Log.d("Test", "Host Button clicked"));
+        binding.menuJoin.setOnClickListener(view -> Log.d("Test", "Join Button clicked"));
+        
         String channelId = "fcm_default_channel";
         String channelName = "Topic";
         /* notificationManager = getSystemService(NotificationManager::class.java)
