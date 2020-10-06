b93e36914898d2143c714208e7e6dede147fcc78
Mohammed Alessa <mohammed.alessa@student.unimelb.edu.au>
2020-10-06 16:52:54 +1100

    first push of two new games. landscape and cleardanger

---
+++ app/src/main/java/com/example/letsparty/Landscape.java
@@ -0,0 +1,16 @@
+package com.example.letsparty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Landscape extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landscape);
+    }
+}
