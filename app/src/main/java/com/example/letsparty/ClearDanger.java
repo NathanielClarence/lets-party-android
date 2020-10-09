de23a61ef04a968b695fc37a36ad2ea5798d94c8
Sharon <sstratsianis@student.unimelb.edu.au>
2020-10-09 19:23:50 +1100

    added firebase token code

--- app/src/main/java/com/example/letsparty/ClearDanger.java
+++ app/src/main/java/com/example/letsparty/ClearDanger.java
@@ -1,17 +1,27 @@
 package com.example.letsparty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class ClearDanger extends AppCompatActivity
 {
+    private static final String TAG = "message" ;
     private int dangerNum = 3;
     AlertDialog.Builder builder;
 
@@ -21,7 +31,26 @@ public class ClearDanger extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clear_danger);
-        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        SharedPreferences prefs = getSharedPreferences("TOKEN_PREF", MODE_PRIVATE);
+        final String token = prefs.getString("token", "");
+
+        Log.e("NEW_INACTIVITY_TOKEN", token);
+
+        if (TextUtils.isEmpty(token)) {
+            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(ClearDanger.this, new OnSuccessListener<InstanceIdResult>() {
+                @Override
+                public void onSuccess(InstanceIdResult instanceIdResult) {
+                    String newToken = instanceIdResult.getToken();
+                    Log.e("newToken", newToken);
+                    SharedPreferences.Editor editor = getSharedPreferences("TOKEN_PREF", MODE_PRIVATE).edit();
+                    if (token != null) {
+                        editor.putString("token", newToken);
+                        editor.apply();
+                    }
+
+                }
+            });
+        }
     }
 
     public void onButtonClicked(View v)
