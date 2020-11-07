fcfc652c0f2f08cdc34c0de43d8d07736e068ac5
Nathaniel Clarence <nathaniel.clarence@ti.ukdw.ac.id>
2020-11-07 13:20:40 +0900

    merge on result

--- app/src/main/java/com/example/letsparty/games/MeasureVoice.java
+++ app/src/main/java/com/example/letsparty/games/MeasureVoice.java
@@ -1,6 +1,7 @@
 package com.example.letsparty.games;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +16,8 @@ import java.util.concurrent.TimeUnit;
 
 public class MeasureVoice extends Game {
 
-    MediaRecorder recorder = new MediaRecorder();
+    private MediaRecorder recorder = new MediaRecorder();
+    private static final String TAG = "MEASURE_VOICE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,12 +45,12 @@ public class MeasureVoice extends Game {
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         recorder.setOutputFile("/dev/null");
-
         try {
             recorder.prepare();
             recorder.start();
+            Log.e(TAG, "Recording user voice started");
             recorder.getMaxAmplitude();
-            System.out.println("Start Recording");
+
         }catch(IOException e){
             e.printStackTrace();
         }catch(SecurityException e){
@@ -59,8 +61,8 @@ public class MeasureVoice extends Game {
     public void end()
     {
         int maxAmp = recorder.getMaxAmplitude();
+        Log.e(TAG, "Amplitude is: " + maxAmp);
         recorder.stop();
-        System.out.println("**********AMP is: " + maxAmp);
         recorder.reset();
         recorder.release();
         System.out.println("recorded");
