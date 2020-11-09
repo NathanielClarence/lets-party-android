9760780b0991e28528e47917bab6165cdb13238f
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2020-11-09 18:19:53 +1100

    Make the GameRunner more durable against duplicate game errors by setting up flags for each whether it has started or not

--- app/src/main/java/com/example/letsparty/activities/GameRunner.java
+++ app/src/main/java/com/example/letsparty/activities/GameRunner.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,24 +19,26 @@ import com.example.letsparty.entities.Room;
 import com.example.letsparty.games.Game;
 import com.example.letsparty.serverconnector.ServerConnector;
 import com.example.letsparty.serverconnector.ServerUtil;
+import com.google.android.gms.tasks.CancellationTokenSource;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GameRunner extends AppCompatActivity {
     private Room room;
     private Player player;
     private List<String> gameIds;
+    private boolean[] gameStarted;
     TextView scores;
     TextView winnerText;
     private boolean isWaiting = false;
     private int currentGameIndex = 0;
     private BroadcastReceiver br;
     private TaskCompletionSource<Boolean> tcs;
+    private CancellationTokenSource cts = new CancellationTokenSource();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,6 +51,8 @@ public class GameRunner extends AppCompatActivity {
         Intent intent = getIntent();
         this.room = (Room) intent.getSerializableExtra(MainActivity.ROOM);
         this.gameIds = intent.getStringArrayListExtra("gameIds");
+        this.gameStarted = new boolean[this.gameIds.size()];
+        Arrays.fill(this.gameStarted, false);
         this.player = (Player) intent.getSerializableExtra(MainActivity.PLAYER);
 
         //only start the game if the state isn't recreated
@@ -66,6 +70,7 @@ public class GameRunner extends AppCompatActivity {
         //start the game
         Intent intent = new Intent(this, gameClass);
         intent.putExtra(MainActivity.ROOM, room);
+        gameStarted[i] = true;
         startActivityForResult(intent, i);
     }
 
@@ -98,17 +103,17 @@ public class GameRunner extends AppCompatActivity {
                     isWaiting = false;
                     if (i < this.gameIds.size()) {
                         //if there are games remaining, go to next game
-                        Timer timer = new Timer();
-                        timer.schedule(new TimerTask()
-                        {
 
-                            public void run()
-                            {
-                                runGame(i);
-                            }
+                        //handle errors of multiple broadcast results
+                        //if the game has started, then don't start it again
+                        if (gameStarted[i])
+                            return;
 
-                        }, 3000);
-
+                        //start the next game after a few seconds so user can see the results
+                        new Handler().postDelayed(
+                                () -> runGame(i),
+                                3000
+                        );
                     } else {
                         //if no games remaining, go to result screen
                         Intent intent = new Intent(this, Results.class);
@@ -120,7 +125,7 @@ public class GameRunner extends AppCompatActivity {
     }
 
     private Task<Boolean> waitForNextGame(){
-        tcs = new TaskCompletionSource<>();
+        tcs = new TaskCompletionSource<>(cts.getToken());
 
         LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(this);
         br = new BroadcastReceiver()
@@ -156,13 +161,14 @@ public class GameRunner extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         outState.putStringArrayList("gameIds", new ArrayList<>(this.gameIds));
+        outState.putBooleanArray("gameStarted", this.gameStarted);
         outState.putBoolean("waiting", isWaiting);
         outState.putInt("currentGameIndex", currentGameIndex);
         outState.putString("winnerText", winnerText.getText().toString());
         outState.putString("scoreText", scores.getText().toString());
         if (isWaiting) {
             LocalBroadcastManager.getInstance(this).unregisterReceiver(br);
-            tcs.setException(new RuntimeException());
+            cts.cancel();
         }
         super.onSaveInstanceState(outState);
     }
@@ -171,6 +177,7 @@ public class GameRunner extends AppCompatActivity {
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         this.gameIds = savedInstanceState.getStringArrayList("gameIds");
+        this.gameStarted = savedInstanceState.getBooleanArray("gameStarted");
         this.winnerText.setText(savedInstanceState.getString("winnerText"));
         this.scores.setText(savedInstanceState.getString("scoreText"));
 
